c691ce129acbc725379444e820e89b012c4d09db
znarcissusz <31096166+znarcissusz@users.noreply.github.com>
2021-01-17 17:03:56 +0800

    add haha into Strategy (#1)
    Co-authored-by: brokenbeast <13466366075@163.com>

--- Strategy.py
+++ Strategy.py
@@ -1,33 +1,39 @@
-from abc import ABCMeta,abstractmethod
+from abc import ABCMeta, abstractmethod
+# haha
+
 
 class Strategy(metaclass=ABCMeta):
     @abstractmethod
-    def excute(self,data):
+    def excute(self, data):
         pass
 
+
 class FastStrategy(Strategy):
-    def excute(self,data):
-        print('using fast method%s'%data)
+    def excute(self, data):
+        print('using fast method%s' % data)
+
 
 class SlowStrategy(Strategy):
-    def excute(self,data):
-        print('using slow method%s'%data)
+    def excute(self, data):
+        print('using slow method%s' % data)
+
 
 class Context:
-    def __init__(self,strategy,data):
-        self.strategy= strategy
+    def __init__(self, strategy, data):
+        self.strategy = strategy
         self.data = data
 
-    def set_strategy(self,strategy,data):
-        self.strategy= strategy
+    def set_strategy(self, strategy, data):
+        self.strategy = strategy
         self.data = data
 
     def Do_strategy(self):
         self.strategy.excute(self.data)
 
+
 s1 = FastStrategy()
 s2 = SlowStrategy()
-c1 = Context(s1,'hahaha')
+c1 = Context(s1, 'hahaha')
 c1.Do_strategy()
-c1.set_strategy(s2,'hehehe')
+c1.set_strategy(s2, 'hehehe')
 c1.Do_strategy()
